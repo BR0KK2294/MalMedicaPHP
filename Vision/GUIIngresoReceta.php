d47a7f029fb3f8a987fb190a988768bdb4de8dde
BR0KK2294 <pablot2294@gmail.com>
2017-06-19 14:09:26 -0400

    Proyecto Base

---
+++ Vision/GUIIngresoReceta.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+<title>Página de bienvenida</title>
+</head>
+
+<body>
+<div class="container-fluid" style="background-color:#7ABDFF">
+  <header id="navegador" class="container">
+    <img src="../Imagenes/Logo-MalMedica.png" />
+    <ul class="nav nav-pills">
+      <li role="presentation" ><a href="Home.html">Home</a></li>
+      <li role="presentation" class="active" ><a href="#">ingreso Receta</a></li>
+      <li role="presentation" ><a href="Mantenedor.html">Mantenedor</a></li>
+      <li role="presentation" ><a href="GUIConsulta.php">Consultas</a></li>
+    </ul>
+  </header>
+</div>
+<div class="container">
+  <section class="row">
+    <h3> Desc. Farmacia - programa</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+  </section>
+</div>
+
+<footer class="container-fluid" style="background-color:#7ABDFF">
+  <div class="container">
+    <p> Contactenos </p>
+  </div>
+</footer>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ Vision/GUIUsuarios.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Lista de Usuarios </h1>
+  <table>
+    <thead>
+      <th>1</th><th>2</th><th>3</th><th>4</th>
+    </thead>
+    <tbody>
+      <tr><td>v1</td><td>v2</td><td>v3</td><td>v4</td></tr>
+    </tbody>
+  </table>
+</body>
+</html>
