51a315386ed8e097d790d7556d3c8711159b647e
BR0KK2294 <pablot2294@gmail.com>
2017-06-30 15:10:04 -0400

    Antes del Merge Pablo

---
+++ Vision/Navegador.php
@@ -0,0 +1,9 @@
+<div id="navegador" class="container">
+  <img src="../Imagenes/Logo-MalMedica.png" />
+  <ul class="nav nav-pills">
+    <li role="presentation" class="active"><a href="">Home</a></li>
+    <li role="presentation" ><a href="GUIIngresoReceta.php">ingreso Receta</a></li>
+    <li role="presentation" ><a href="Mantenedor.html">Mantenedor</a></li>
+    <li role="presentation" ><a href="GUIConsulta.php">Consultas</a></li>
+  </ul>
+</div>
